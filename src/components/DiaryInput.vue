0a8fd7679d40372fe76baf031e37fa2fa1523a6e
dev-axnn <dev.axnn@gmail.com>
2022-08-30 11:08:54 +0900

    애니메이션 적용

--- src/components/DiaryInput.vue
+++ src/components/DiaryInput.vue
@@ -1,59 +1,74 @@
 <template>
   <div class="input-wrap shadow">
     <input type="text" v-model="newItem" class="input-box" maxlength="30" @keyup.enter="addItem">
-    <!-- <button v-on:click="addItem">등록</button> -->
-    <span @click="addItem" class="add-bt">
-      <i class="fas fa-plus add-bt-icon"></i>
-    </span>
+    <div class="option">
+      <span @click="addIcon(0)" class="img1"> 
+        이미지1     
+      </span>
+      <span @click="addIcon(1)" class="img2">  
+        이미지2         
+      </span>
+      <span @click="addIcon(2)" class="img3">  
+        이미지2         
+      </span>
+      <span @click="addItem" class="add-bt">
+        <i class="fas fa-plus add-bt-icon"></i>
+      </span>
+    </div>
+    <!-- 안내창 -->
+    <ModalView :show="showModal" v-on:closemodal="showModal=false">
+      <template #header>
+        <h3>안내창</h3>
+      </template>
+      <template #body>
+        <h2>내용을 작성하셔야 합니다.</h2>
+      </template>
+    </ModalView>
   </div>
 </template>
 
 <script>
   import { ref } from 'vue';
+  import ModalView from '@/components/common/ModalView.vue';
 
   export default {
-    setup(){
+    components: { 
+      ModalView 
+    },
+    setup(props, context){
       const newItem = ref('');
-
-      // 현재 시간값을 계산하여 '중복되지 않는 id'로 사용한다.
-      // 10보다 작은 값에 0을 붙임
-      const addZero = (n) => {
-        return n < 10 ? '0' + n : n;
-      }
-      // 현재 시간을 리턴
-      const getCurrentDate = () => {
-        let date = new Date();
-        return date.getFullYear().toString() + addZero(date.getMonth() + 1) + addZero(date.getDate()) +
-          addZero(date.getHours()) + addZero(date.getMinutes()) + addZero(date.getSeconds());              
-      }
+      const newIcon = ref(0);
+      const showModal = ref(false);
 
       const addItem = () => {
         let temp = newItem.value;
+        let icon = newIcon.value;
         // 앞쪽 뒷쪽 공백 제거
         temp = temp.trim();
-        // 앞자리 공백, 뒷자리 공백 제거해주는 코드 추후 업데이트 예정
-        if(temp !== ''){
-          // localStorage.setItem(키.값 (json 형태로 저장 및 업데이트 예정 JSON.stringify(object)))
-          // ('localStorage', 'setItem' 은 고정값.)
-          let memoTemp = {
-            id: getCurrentDate(),
-            complete: false,
-            memotitle: newItem.value
-          };
-
-          // 추후 DB 연동 예정
-          localStorage.setItem(memoTemp.id, JSON.stringify(memoTemp));
+        // 추후 업데이트 예정(정규표현식-문자열체크 문법)
+        //  앞자리공백   공백    뒷자리공백
+        if (temp !== '') {          
+          context.emit("additem", temp, icon);
           resetItem();
+        }else{
+          showModal.value = true;
         }
       }
+
+      // 내용 재설정
       const resetItem = () => {
         newItem.value = '';
+      }
+
+      const addIcon = (index) => {
+        newIcon.value = index;
       }
 
       return{
         newItem,
         addItem,
-        resetItem
+        addIcon,
+        showModal
       }
     }
   }
@@ -86,10 +101,50 @@
     margin-left: 20px;
   }
 
-  .add-bt {
+  .option {
+    position: absolute;
     display: block;
-    float: right;
-    background: hotpink;
+    right: 0;
+    top: 0;
+  }
+
+  .img1:active,
+  .img2:active,
+  .img3:active {
+    outline: 3px solid hotpink;
+  }
+
+  .img1 {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    font-size: 0;
+    cursor: pointer;
+    background: url('@/assets/images/dog1.png') no-repeat center;
+    background-size: cover;
+  }
+  .img2 {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    font-size: 0;
+    cursor: pointer;
+    background: url('@/assets/images/dog2.png') no-repeat center;
+    background-size: cover;
+  }
+  .img3 {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    font-size: 0;
+    cursor: pointer;
+    background: url('@/assets/images/str.png') no-repeat center;
+    background-size: cover;
+  }
+
+  .add-bt {
+    display: inline-block;
+    background-color: hotpink;
     cursor: pointer;
   }
 
--- src/components/DiaryList.vue
+++ src/components/DiaryList.vue
@@ -1,60 +1,36 @@
 <template>
   <div class="list-wrap">
-    <ul>
+    <TransitionGroup name="list" tag="ul">
       <!-- v-for"in" :key -->
-      <li v-for="(item, index) in memoItemArr" :key="index" class="shadow">
-        <i class="fas fa-check-square check-bt" @click="updateMemo(item)" v-bind:class="{completeCheck:item.complete}"></i>
+      <li v-for="(item, index) in memodata" :key="index" class="shadow">
+        <i class="fas fa-check-square check-bt" @click="updateMemo(item, index)" v-bind:class="{completeCheck:item.complete}"></i>
         <span :class="{completeCheckTxt:!item.complete}">{{item.memotitle}}</span>
-        <span class="remove-bt" @click="removeMemo(item.id, index)">
-          <i class="fas fa-times"></i>
-        </span>
+        <div class="info">
+          <span class="icon" :style="{backgroundImage:'url(' + require(`@/assets/images/${item.memoicon}`) + ')'}"></span>            
+          <span class="date">{{item.memodate}}</span>
+          <span class="remove-bt" @click="removeMemo(item.id, index)">
+            <i class="fas fa-times"></i>
+          </span>
+        </div>
       </li>
-    </ul>
+    </TransitionGroup>
   </div>
 </template>
 
 <script>
-  import { ref, reactive } from "vue";
-
   export default {
-    setup(){
-      // localstorage 의 목록을 가지고 오기
-      // 전체 개수 
-      const total = ref(0);
-      total.value = localStorage.length;
-      console.log(total.value);
-
-      // Keyname 을 저장하는 배열
-      const memoItemArr = reactive([]);
-      
-      if(total.value > 0){
-        for(let i = 0; i < total.value; i++){
-          // memoItemArr 배열에 데이터 담기
-          // 날짜시간을 id 값으로 가진 실제 item.title 나타내기
-          // 추후 DB 연동 예정
-          let obj = localStorage.getItem(localStorage.key(i));
-          memoItemArr.push(JSON.parse(obj));
-        }
-        // Key 값을 이용하여 정렬(오름차순)
-      }
+    props: [ 'memodata' ],
+    setup(props, context) {
 
       const removeMemo = (item, index) => {
-        // localStrage 에서 key를 통해 삭제.
-        localStorage.removeItem(item);
-        // 배열(memoItemArr) 에서도 삭제. (splice : 시작점으로부터 갯수 설정하여 삭제해주는 명령어)
-        memoItemArr.splice(index, 1);
+        context.emit('removeitem', item, index);
       }
 
-      const updateMemo = (item) => {
-        // localStorage 에서는 update method 를 지원하지 않는다.
-        // 찾아서 지우고, 다시 set 한다.
-        localStorage.removeItem(item.id);
-        item.complete = !item.complete;
-        localStorage.setItem(item.id, JSON.stringify(item));
+      const updateMemo = (item, index) => {      
+        context.emit("updateitem", item, index);
       }
 
       return{
-        memoItemArr,
         removeMemo,
         updateMemo
       }
@@ -72,6 +48,20 @@
     border-radius: 5px;
     padding-left: 10px;
   }
+
+  .info {
+    margin-left: auto;
+  }
+  .icon {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;  
+  }
+  .date {}
 
   .remove-bt {
     cursor: pointer;
@@ -98,5 +88,16 @@
   .completeCheckTxt {
     color: #666;
     text-decoration: line-through;
+  }
+
+  /* Animation */
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.5s ease;
+  }
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
   }
 </style>
